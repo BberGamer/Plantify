2b1b498d1e4dd7ee1a0587f0f150c7748fb0a9b0
kiet493 <ngoanhkiet138@gmail.com>
2026-06-09 20:02:17 +0700

    feat(products): thêm product service với getProductById

--- BE/src/features/products/product.service.js
+++ BE/src/features/products/product.service.js
@@ -1,1 +1,17 @@
 // product.service.js - Business logic cho Products
+const Product = require('./product.model');
+
+/**
+ * Lấy product theo id, populate category
+ * @param {string} id - Product id
+ * @returns {Promise<Object>} Product object
+ */
+async function getProductById(id) {
+  const product = await Product.findById(id).populate('categoryId');
+  if (!product) {
+    throw new Error('Product not found');
+  }
+  return product;
+}
+
+module.exports = { getProductById };
