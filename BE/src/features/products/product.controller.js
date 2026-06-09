e2655148fc57ef93a1afcb20f5cd5e8559f52366
kiet493 <ngoanhkiet138@gmail.com>
2026-06-09 20:04:40 +0700

    feat(products): thêm product controller, route GET /:id và register API

--- BE/src/features/products/product.controller.js
+++ BE/src/features/products/product.controller.js
@@ -1,1 +1,13 @@
 // product.controller.js - Xử lý request/response cho Products
+const productService = require('./product.service');
+
+async function getProductById(req, res, next) {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    res.json({ success: true, message: 'Lấy sản phẩm thành công', data: product });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { getProductById };
--- BE/src/features/products/product.routes.js
+++ BE/src/features/products/product.routes.js
@@ -1,1 +1,8 @@
 // product.routes.js - Định nghĩa các route cho Products
+const express = require('express');
+const router = express.Router();
+const productController = require('./product.controller');
+
+router.get('/:id', productController.getProductById);
+
+module.exports = router;
