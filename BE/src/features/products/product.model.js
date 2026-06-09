7f53a150ab846bbe4adb1f8c578b651beffe97fe
kiet493 <ngoanhkiet138@gmail.com>
2026-06-09 20:01:12 +0700

    feat(products): thêm product model và product-category model

--- BE/src/features/products/product.model.js
+++ BE/src/features/products/product.model.js
@@ -1,1 +1,69 @@
 // product.model.js - Mongoose schema cho Product
+const mongoose = require('mongoose');
+const ProductCategory = require('./product-category.model');
+
+const productSchema = new mongoose.Schema(
+  {
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ProductCategory',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    thumbnail: {
+      type: String,
+      trim: true,
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    brand: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    ratingAverage: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    ratingCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'products',
+  }
+);
+
+module.exports = mongoose.model('Product', productSchema);
