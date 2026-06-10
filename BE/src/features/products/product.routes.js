6a2e218244cd220c321b3ea28e87d27856f81fab
binhdvhe182022 <binhdvhe182022@fpt.edu.vn>
2026-06-10 21:38:19 +0700

    feat: triển khai API danh sách sản phẩm và danh mục cho marketplace

--- BE/src/features/products/product.routes.js
+++ BE/src/features/products/product.routes.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 const productController = require('./product.controller');
 
+router.get('/', productController.getAllProducts);
+router.get('/categories', productController.getAllCategories);
 router.get('/:id', productController.getProductById);
 
 module.exports = router;
+
