57ec2669d47862cd3e6d2fcf0e57c027344d45ab
binhdvhe182022 <binhdvhe182022@fpt.edu.vn>
2026-06-10 21:38:48 +0700

    feat: kết nối trang marketplace với API và các bộ lọc động

--- FE/src/features/products/hooks/index.js
+++ FE/src/features/products/hooks/index.js
@@ -1,2 +1,4 @@
 // hooks/index.js - Export tất cả hooks của products
-export { useProduct, default } from "./useProduct";
+export { useProduct } from "./useProduct";
+export { useProducts } from "./useProducts";
+
